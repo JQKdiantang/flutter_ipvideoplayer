91c99a68acb983ae4e92b5048921cabd654cd6fc
JQKdiantang <97504607+JQKdiantang@users.noreply.github.com>
2026-03-04 18:16:55 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# flutter_ipvideoplayer
-该项目为安卓前端项目，使用flutter构造，引用media_kit模块做为播放器，目前只支持配套的后端golang进行适配播放。播放器只具备正常播放快进等基础播放，
+# deomotwo_video
+
+two
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "K:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "K:\\flutterdemo\\deomotwo_video" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.4" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 4 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=K:\\flutter"
+  "PROJECT_DIR=K:\\flutterdemo\\deomotwo_video"
+  "FLUTTER_ROOT=K:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=K:\\flutterdemo\\deomotwo_video\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=K:\\flutterdemo\\deomotwo_video"
+  "FLUTTER_TARGET=K:\\flutterdemo\\deomotwo_video\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9WRVJTSU9OPTMuMzIuNw==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZDdiNTIzYjM1Ng==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MzlkNmQ2ZTY5OQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My44LjE="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=K:\\flutterdemo\\deomotwo_video\\.dart_tool\\package_config.json"
+)
